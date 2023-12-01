4545c4c9326f663bdb0b8a3a19eb0e5f182b7bcb
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-12-01 15:14:49 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,7 +1,7 @@
 local function AutoFight(world,enemy) -- world = 1, enemy = 2
     game:GetService("ReplicatedStorage")["Events"]["AutoFight"]:FireServer("Fight",world,enemy)
 end
--- WAIT
+
 local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/NMEHkVTb"))()
 local Window = OrionLib:MakeWindow({Name = "VIP Turtle Hub V3", HidePremium = false, SaveConfig = false, ConfigFolder = "TurtleFi"})
 
@@ -11,6 +11,22 @@ Icon = "rbxassetid://4483345998",
 PremiumOnly = false
 })
 
+local T2 = Window:MakeTab({
+Name = "Hatch",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false
+})
+
+T2:AddLabel("Coming Soon!")
+
+local T3 = Window:MakeTab({
+Name = "Dungeon",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false
+})
+
+T3:AddLabel("Coming Soon!")
+
 T1:AddToggle({
   Name = "Auto Punch",
   Default = false,
@@ -18,7 +34,30 @@ T1:AddToggle({
     _G.p = Value
       while wait() do
       if _G.p == false then break end
-        
+        game:GetService("ReplicatedStorage")["Events"]["DamageIncreaseOnClickEvent"]:FireServer()
+      end
+  end    
+})
+
+T1:AddToggle({
+  Name = "Auto Claim Gifts",
+  Default = false,
+  Callback = function(Value)
+    _G.g = Value
+      while wait() do
+      if _G.g == false then break end
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(1)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(2)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(3)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(4)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(5)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(6)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(7)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(8)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(9)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(10)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(11)
+        game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(12)
       end
   end    
 })
