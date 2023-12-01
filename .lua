b29ded224591bf8552b7ecf26d1f13615a0cb358
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-12-01 15:47:22 +0900

    Update .lua

--- .lua
+++ .lua
@@ -7,25 +7,104 @@ local Window = OrionLib:MakeWindow({Name = "VIP Turtle Hub V3", HidePremium = fa
 
 local T1 = Window:MakeTab({
 Name = "Main",
-Icon = "rbxassetid://4483345998",
+Icon = "rbxassetid://",
 PremiumOnly = false
 })
 
 local T2 = Window:MakeTab({
 Name = "Hatch",
-Icon = "rbxassetid://4483345998",
+Icon = "rbxassetid://",
 PremiumOnly = false
 })
 
-T2:AddLabel("Coming Soon!")
-
 local T3 = Window:MakeTab({
 Name = "Dungeon",
-Icon = "rbxassetid://4483345998",
+Icon = "rbxassetid://",
+PremiumOnly = false
+})
+
+local T4 = Window:MakeTab({
+Name = "Fighting",
+Icon = "rbxassetid://",
 PremiumOnly = false
 })
 
 T3:AddLabel("Coming Soon!")
+
+T4:AddTextbox({
+  Name = "World Number",
+  Default = "1",
+  TextDisappear = false,
+  Callback = function(Value)
+    _G.AsyncWorld = tonumber(Value)
+  end  
+})
+
+T4:AddTextbox({
+  Name = "Enemy Number",
+  Default = "1",
+  TextDisappear = false,
+  Callback = function(Value)
+    _G.AsyncEnemy = tonumber(Value)
+  end  
+})
+
+T4:AddToggle({
+  Name = "FIGHT",
+  Default = false,
+  Callback = function(Value)
+    if Value then
+      AutoFight(_G.AsyncWorld,_G.AsyncEnemy)
+    else
+      game:GetService("ReplicatedStorage")["Events"]["AutoFight"]:FireServer("StopFight")
+    end
+  end    
+})
+
+T2:AddTextbox({
+  Name = "Egg number",
+  Default = "1",
+  TextDisappear = false,
+  Callback = function(Value)
+    _G.AsyncEgg = Value
+  end  
+})
+
+T2:AddToggle({
+  Name = "Auto Hatch",
+  Default = false,
+  Callback = function(Value)
+    _G.ht = Value
+      while wait() do
+      if _G.ht == false then break end
+        game:GetService("ReplicatedStorage")["Events"]["PlayerPressedKeyOnEgg"]:FireServer(_G.AsyncEgg)
+      end
+  end    
+})
+
+T2:AddToggle({
+  Name = "Auto Winter Egg",
+  Default = false,
+  Callback = function(Value)
+    _G.htwe = Value
+      while wait() do
+      if _G.htwe == false then break end
+        game:GetService("ReplicatedStorage")["Events"]["PlayerPressedKeyOnEgg"]:FireServer("WinterEgg")
+      end
+  end    
+})
+
+T2:AddToggle({
+  Name = "Auto Equip Best",
+  Default = false,
+  Callback = function(Value)
+    _G.eqb = Value
+      while wait() do
+      if _G.eqb == false then break end
+        game:GetService("ReplicatedStorage")["Events"]["EquipBest"]:InvokeServer()
+      end
+  end    
+})
 
 T1:AddToggle({
   Name = "Auto Punch",
@@ -60,4 +139,28 @@ T1:AddToggle({
         game:GetService("ReplicatedStorage")["Events"]["PlaytimeRewardUpdateEvent"]:FireServer(12)
       end
   end    
+})
+
+T1:AddToggle({
+  Name = "Auto Claim Task",
+  Default = false,
+  Callback = function(Value)
+    _G.task = Value
+      while wait() do
+      if _G.task == false then break end
+        game:GetService("ReplicatedStorage")["Events"]["QuestEvent"]:InvokeServer("Claim")
+      end
+  end    
+})
+
+T1:AddToggle({
+  Name = "Auto Ascend",
+  Default = false,
+  Callback = function(Value)
+    _G.asc = Value
+      while wait() do
+      if _G.asc == false then break end
+        game:GetService("ReplicatedStorage")["Events"]["AscendEvent"]:FireServer(true)
+      end
+  end
 })
