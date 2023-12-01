f2b35cb6515d30e866b305839dd0c5eb11536191
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-12-01 15:09:15 +0900

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,24 @@
+local function AutoFight(world,enemy) -- world = 1, enemy = 2
+    game:GetService("ReplicatedStorage")["Events"]["AutoFight"]:FireServer("Fight",world,enemy)
+end
+-- WAIT
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/NMEHkVTb"))()
+local Window = OrionLib:MakeWindow({Name = "VIP Turtle Hub V3", HidePremium = false, SaveConfig = false, ConfigFolder = "TurtleFi"})
+
+local T1 = Window:MakeTab({
+Name = "Main",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false
+})
+
+T1:AddToggle({
+  Name = "Auto Punch",
+  Default = false,
+  Callback = function(Value)
+    _G.p = Value
+      while wait() do
+      if _G.p == false then break end
+        
+      end
+  end    
+})
